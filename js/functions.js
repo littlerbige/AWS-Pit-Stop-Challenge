6cc1e7da38378dbfa28db7df9c58c7acc874add3
Kyle Murphy <kmurphy@revolutionworld.com>
2024-04-23 12:12:16 -0400

    feat: moved classes back to main functions file

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,69 @@
-import scenario from "./modules/scenario.js";
-import tyre from "./modules/tyres.js";
+//import scenario from "./modules/scenario.js";
+//import tyre from "./modules/tyres.js";
+
+class scenario {
+    constructor(lapNum, currTyres, weather, tyres, correctMessage, incorrectMessage) {
+        this._lapNum = lapNum;
+        this._currTyres = currTyres;
+        this._weather = weather;
+        this._tyres = tyres;
+        this._correctMessage = correctMessage;
+        this._incorrectMessage = incorrectMessage;
+    }
+
+    get lapNum() {
+        return this._lapNum;
+    }
+
+    get currTyres() {
+        return this._currTyres;
+    }
+
+    get weather() {
+        return this._weather;
+    }
+
+    get tyres() {
+        return this._tyres;
+    }
+
+    get correctMessage() {
+        return this._correctMessage;
+    }
+
+    get incorrectMessage() {
+        return this._incorrectMessage;
+    }
+
+    getTyreByName(name) { 
+        this._tyres.forEach(tyre => {
+            if(tyre.name == name){
+                return tyre;
+                exit;
+            }
+        });
+     }
+}
+
+export default class tyre {
+    constructor(name, laps, penalty){
+        this._name = name;
+        this._laps = laps;
+        this._penalty = penalty;
+    }
+
+    get name(){
+        return this._name;
+    }
+
+    get laps(){
+        return this._laps;
+    }
+
+    get penalty(){
+        return this._penalty;
+    }
+}
 
 document.getElementById("start-game-btn").addEventListener("click", startGame);
 document.getElementById("start-timer-btn").addEventListener("click", startTimer);
