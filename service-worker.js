8cd8834cd25d56230d3b62f41590b75b197f891f
Kyle Murphy <kmurphy@revolutionworld.com>
2024-04-23 19:12:26 -0400

    fix: adding video precaching

--- service-worker.js
+++ service-worker.js
@@ -15,56 +15,57 @@ Copyright 2021 Google LLC
  */
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
-import {registerRoute} from 'workbox-routing';
-import {CacheFirst} from 'workbox-strategies';
-import {CacheableResponsePlugin} from 'workbox-cacheable-response';
-import {RangeRequestsPlugin} from 'workbox-range-requests';
 
-import {imageCache} from 'workbox-recipes';
-
-imageCache();
-
-registerRoute(
-     ({request}) => {
-       const {destination} = request;
-   
-       return destination === 'video'
-     },
-     new CacheFirst({
-       cacheName: 'video-cache',
-       plugins: [
-         new CacheableResponsePlugin({
-           statuses: [200]
-         }),
-         new RangeRequestsPlugin(),
-       ],
-     }),
-   );
-
- // Choose a cache name
-const cacheName = 'cache-v1';
-// List the files to precache
-const precacheResources = ['/', '/index.html', '/styles/style.css', '/js/functions.js'];
+const cacheName = 'v2-assets'
+const precacheResources = ['/', '/index.html', '/styles/style.css', '/js/functions.js', '/assets/imgs/F1_RM_AWS_Technology_Lockup1_Hzn_White_Standard_CMYK 1.png', 'assets/imgs/section-bg.png', 'assets/videos/intro.mp4'];
 
 // When the service worker is installing, open the cache and add the precache resources to it
 self.addEventListener('install', (event) => {
-  console.log('Service worker install event!');
-  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
+     console.log('Service worker install event!');
+     event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
 });
+
+workbox.routing.registerRoute(
+     ({request}) => {
+          const {destination} = request;
+          return destination === 'video';
+     },
+     new workbox.strategies.CacheFirst({
+          cacheName: cacheName,
+          plugins: [
+          new workbox.cacheableResponse.CacheableResponsePlugin({
+               statuses: [0, 200]
+          }),
+          new workbox.rangeRequests.RangeRequestsPlugin(),
+          ],
+     }),
+);
 
 self.addEventListener('activate', (event) => {
-  console.log('Service worker activate event!');
+     console.log('Service worker activate event!');
+     //remove unwanted caches
+     event.waitUntil(
+          caches.keys().then(cacheNames =>{
+               return Promise.all(
+                    cacheNames.map(cache => {
+                         if(cache !== cacheName){
+                              console.log('Service Worker: Clearing Old Cache');
+                              return caches.delete(cache);
+                         }
+                    })
+               )
+          })
+     );
 });
-
 // When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
 self.addEventListener('fetch', (event) => {
-  console.log('Fetch intercepted for:', event.request.url);
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      if (cachedResponse) {
-        return cachedResponse;
-      }
-      return fetch(event.request);
-    }),
-  );
+     console.log('Fetch intercepted for:', event.request.url);
+     event.respondWith(
+          caches.match(event.request).then((cachedResponse) => {
+          if (cachedResponse) {
+               return cachedResponse;
+          }
+          return fetch(event.request);
+          }),
+     );
 });
